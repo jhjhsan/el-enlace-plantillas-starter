55a703b5611b8139e5fff830a2dbffd49224dca1
jhjhsan <santana.autopartes1@gmail.com>
2025-12-31 15:28:21 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // 🚫 desactiva "media" y fuerza solo por clase
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
