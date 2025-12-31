eb66486b98e3feaef68046cd5ccee32c730f6655
jhjhsan <santana.autopartes1@gmail.com>
2025-12-31 16:06:11 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false, // 🔒 Desactiva modo oscuro global
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#D8A353",
-        accent: "#00BFFF",
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
